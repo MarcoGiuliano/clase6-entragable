0da2859d7d500a2d139fd03a6dfa7012c7bb04f6
Giuliano <giulianoperrinidev@gmail.com>
2022-05-10 17:21:28 -0300

    push tarea

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import CategoriesScreen from "./Screens/CategoriesScreen";
+import { useState } from "react";
+import Products from "./Screens/Products";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [categorySelected, setcategorySelected] = useState(null);
+
+  const handleCategory = (category) => {
+    setcategorySelected(category);
+  };
+
+  const [loaded] = useFonts({
+    Poppins: require("./assets/fonts/Poppins-Medium.ttf"),
+  });
+
+  if (!loaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {categorySelected ? (
+        <Products category={categorySelected} />
+      ) : (
+        <CategoriesScreen handleCategory={handleCategory} />
+      )}
     </View>
   );
 }
@@ -13,8 +33,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
